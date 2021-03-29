012de992d1dd0b290f4d0717966ebe053f7bc3fb
Rohit kumar pandey <80757398+pandeyrohit1234@users.noreply.github.com>
2021-03-29 03:49:08 -0700

    Calculator_Delegate

---
+++ Lab-12/Calculator_Delegate/Calculator_Delegate/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator_Delegate
+{
+    class Calculator
+    {
+        public int n1 { get; set; }
+        public int n2 { get; set; }
+    }
+    public int sub(int n1, int n2)
+    {
+        int ans = n1 - n2;
+        return ans;
+    }
+}
---
+++ Lab-12/Calculator_Delegate/Calculator_Delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculator_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
